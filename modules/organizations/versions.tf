fdafed5bbe6fd00b978d5c4913f592c62fae8c31
mamss00 <sallmamadou91@gmail.com>
2025-01-25 18:42:15 +0100

    Fix provider version

--- modules/organizations/versions.tf
+++ modules/organizations/versions.tf
@@ -1,11 +1,11 @@
 # Specify required versions for Terraform and providers
 # AWS provider >= 4.0.0 needed for Organizations and Control Tower features
 terraform {
- required_version = ">= 1.0.0"
+ required_version = "~> 1.10.0"  # Compatible with 1.10.x
  required_providers {
    aws = {
      source  = "hashicorp/aws"
-     version = ">= 4.0.0"
+     version = "~> 5.84.0"  # Compatible with 5.84.x
    }
  }
 }
